2a6554a2c48d56dc1d1b20d9d3a709f9838ec66e
= <=>
2023-02-12 20:28:37 +0530

    example

--- mob/App.jsx
+++ mob/App.jsx
@@ -10,17 +10,81 @@ import React from 'react';
 import {
   Text,
   View,
+  ScrollView,
+  TextInput,
+  Button
 } from 'react-native';
 
-Meteor.connect("wss://192.168.1.5:3000/websocket");
+Meteor.connect("ws://192.168.1.5:3000/websocket");
+
+const Links = new Mongo.Collection("links");
   
 class App extends React.Component {
+  constructor(props){  
+    super(props);  
+    this.state = {
+      title: "",
+      link: ""
+    };
+  }  
+  onClick () {
+    const {title, link} = this.state;
+    Meteor.call('links.insert', title, link, (error) => {
+      if (error) {
+        alert(error.error);
+      } else {
+        this.setState({
+          title: "",
+          link: ""
+        });
+      }
+    });
+  }
   render() {
-      return (
-        <View>
-<Text>Hello</Text>
-        </View>
-      );
+  const { loading, allLinks } = this.props;
+  if(loading) {
+    return <View><Text>Loading your Links...</Text></View>
+  }
+
+  return (
+
+    <View>
+      <TextInput id='title' value={this.state.title} onChangeText={(text) => {
+        console.log("text", text)
+        this.setState({
+          title: text
+        })
+      }}/>
+      <TextInput id='link' value={this.state.link}  onChangeText={(text) => {
+        this.setState({
+          link: text
+        })
+      }}/>
+      <Button onPress={() => this.onClick()} title="Submit"/>
+    <ScrollView>
+        {!allLinks.length ?
+            <Text>You don't have any tasks</Text>
+        :
+        allLinks.map(link => (
+                <Text>{link.title}</Text>
+            ))
+        }
+    </ScrollView>
+    </View>
+    );
   }
 }
 
+ const MyAppContainer = withTracker(() => {
+    
+  const allLinks = Links.find().fetch();
+  const handle = Meteor.subscribe("links.all");
+  
+  return {
+    allLinks,
+    loading:!handle.ready()
+  };
+  
+})(App);
+
+export default MyAppContainer;
