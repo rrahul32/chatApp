0ce4ec9eeae1cac89f32115d5dc20a4e4d3ea854
= <=>
2023-03-12 16:12:49 +0530

    commmit 22

--- server/imports/startup/server/register-api.js
+++ server/imports/startup/server/register-api.js
@@ -1,4 +1,5 @@
 // Register your apis here
 
-import '../../api/links/methods.js';
-import '../../api/links/server/publications.js';
+import '../../api/Chat/methods';
+import '../../api/Chat/publications';
+import '../../api/ChatMessages/methods';
