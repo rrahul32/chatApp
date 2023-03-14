5a5b844f1c9c6cd01e67a68e56e033fc741e2a12
= <=>
2023-03-15 00:37:02 +0530

    set msg limit to 13

--- mob/src/screens/auth/ChatWindow.jsx
+++ mob/src/screens/auth/ChatWindow.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {GiftedChat, Bubble} from 'react-native-gifted-chat';
 import {StyleSheet, View} from 'react-native';
 import Meteor, {withTracker, Mongo} from '@meteorrn/core';
@@ -7,19 +7,38 @@ import Meteor, {withTracker, Mongo} from '@meteorrn/core';
 const ChatMessages = new Mongo.Collection('chatMessages');
 const ChatWindow = ({chatId, loading, messages, userId}) => {
   if (loading) return;
-  console.log('userId: ', userId);
+  // console.log('userId: ', userId);
   // console.log('messages: ' + messages);
-  const chatMessages = messages.map(message => {
-    return {
-      _id: message._id,
-      text: message.text,
-      createdAt: message.createdAt,
-      user: {
-        _id: message.createdBy.id,
-      },
-    };
-  });
+  const [isLoadingEarlier, setIsLoadingEarlier] = useState(false);
+  const [isAllLoaded, setIsAllLoaded] = useState(false);
+  const [msgs, setMsgs]= useState([]);
+  // const chatMessages = messages.map(message => {
+  //   return {
+  //     _id: message._id,
+  //     text: message.text,
+  //     createdAt: message.createdAt,
+  //     user: {
+  //       _id: message.createdBy.id,
+  //     },
+  //   };
+  // });
+
+  // console.log("msg: ", ChatMessages.find().fetch().length)
+
+  useEffect(() => {
+    setMsgs(messages.map(message => {
+      return {
+        _id: message._id,
+        text: message.text,
+        createdAt: message.createdAt,
+        user: {
+          _id: message.createdBy.id,
+        },
+      };
+    }));
+  }, [messages])
   
+  // console.log("lat: ",chatMessages[chatMessages.length-1])
   function renderMessage(props) {
     const isCurrentUser = props.currentMessage.user._id === props.user._id;
 
@@ -51,6 +70,39 @@ const ChatWindow = ({chatId, loading, messages, userId}) => {
     );
   }
 
+  const onLoadEarlier = async () => {
+    if (!isLoadingEarlier && !isAllLoaded) {
+      setIsLoadingEarlier(true);
+
+      const oldestMessage = msgs[msgs.length - 1];
+      const earlierMessages = ChatMessages.find({
+        chatId: {$ne: oldestMessage._id},
+        createdAt: { $lt: oldestMessage.createdAt }
+      }, {
+        sort: { createdAt: -1 },
+        limit: 13
+      }).fetch();
+      // console.log("len: ",earlierMessages);
+      if(earlierMessages.length<10)
+      setIsAllLoaded(true);
+
+      setMsgs((prev)=>[
+        ...prev,
+        ...earlierMessages.map((message)=>({
+          _id: message._id,
+          text: message.text,
+          createdAt: message.createdAt,
+          user: {
+            _id: message.createdBy.id,
+          },
+        }))
+      ]);
+
+      setIsLoadingEarlier(false);
+
+    }
+  }
+
   function onSend(newMessages = []) {
     // console.log(chatId);
     Meteor.call(
@@ -74,15 +126,16 @@ const ChatWindow = ({chatId, loading, messages, userId}) => {
     //   <ChatWindowHeader name="Native" avatar='https://placeimg.com/140/140/any' />
     <GiftedChat
       textInputStyle={styles.input}
-      messages={chatMessages}
+      messages={msgs}
       onSend={newMessages => onSend(newMessages)}
       user={{
         _id: userId,
       }}
       renderAvatar={() => null}
       renderMessage={renderMessage}
-      inverted={false}
-      
+      loadEarlier={!isAllLoaded}
+      onLoadEarlier={onLoadEarlier}
+      isLoadingEarlier={isLoadingEarlier}
     />
     // </View>
   );
@@ -102,7 +155,7 @@ export default withTracker(({route, navigation}) => {
   const chatId = route.params.chatId;
   const handle = Meteor.subscribe('chatMessages', chatId);
   const ready = handle.ready();
-  const messages = ChatMessages.find().fetch();
+  const messages = ChatMessages.find({},{sort: {createdAt: -1}, limit: 13}).fetch();
   const userId = Meteor.user()._id;
   return {
     chatId,
